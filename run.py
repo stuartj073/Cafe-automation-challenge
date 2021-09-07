0a7438ef67163f9e90b5bbb43692319598910959
stuartj073 <stuartj073@gmail.com>
2021-09-07 14:16:23 +0000

    Collect data from last 5 entries in sales worksheet

--- run.py
+++ run.py
@@ -64,6 +64,24 @@ def update_sales_worksheet(data):
     sales_worksheet.append_row(data)
     print("Sales worksheet ipdated succesffully. \n")
 
+def update_surplus_worksheet(data):
+    """
+    Update surplus worksheet, add new row with the list data provided
+    """
+    print("Updating surplus worksheet...\n")
+    surplus_worksheet = SHEET.worksheet("surplus")
+    surplus_worksheet.append_row(surplus_data)
+    print("Surplus worksheet updated succesffully. \n")
+
+def update_worksheet(data, worksheet):
+    """
+    Update all worksheets for given user data
+    """
+    print(f"Updating {worksheet} worksheet....\n")
+    worksheet_to_update = SHEET.worksheet(worksheet)
+    worksheet_to_update.append_row(data)
+    print(f"{worksheet} worksheet updated successfully\n")
+
 
 def calculate_surplus_data(sales_row):
     """
@@ -84,15 +102,21 @@ def calculate_surplus_data(sales_row):
     
     return surplus_data
 
-def update_surplus_worksheet(surplus_data):
-    """
-    Update surplus worksheet, add new row with the list data provided
-    """
-    print("Updating surplus worksheet...\n")
-    surplus_worksheet = SHEET.worksheet("surplus")
-    surplus_worksheet.append_row(surplus_data)
-    print("Surplus worksheet updated succesffully. \n")
 
+def get_last_5_entries_sales():
+    """
+    Collects collumsn of data from worksheet, of last 5 entries for 
+    each sandwich and returns the data as a list of lists
+    """
+    sales = SHEET.worksheet("sales")
+
+
+    columns = []
+    for ind in range(1, 7):
+        column = sales.col_values(ind)
+        columns.append(column[-5:])
+
+    return columns
 
 
 def main():
@@ -101,9 +125,11 @@ def main():
     """
     data = get_sales_data()
     sales_data = [int(num) for num in data]
-    update_sales_worksheet(sales_data)
+    update_worksheet(sales_data, "sales")
     new_surplus_data = calculate_surplus_data(sales_data)
-    update_surplus_worksheet(new_surplus_data)
+    update_worksheet(new_surplus_data, "stock")
 
 print("Welcome to Love Sandwiches Data Automation")
-main()
+# main()
+
+sales_columns = get_last_5_entries_sales()
